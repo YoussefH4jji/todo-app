1908de53bd602512a96a4ee7afee77256ecb8fc9
Youssef Hajji <hajji.youssef.personal@gmail.com>
2024-02-29 12:22:47 +0100

    Update MainPage.jsx

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -5,7 +5,7 @@ export default function MainPage(){
             <h1 className="title">Remind.me</h1>
             <div className="main-page-title-container">
                 <h2 className="main-page-title">Stay Organized,
-                <br/>Stay Relaxed§§§</h2>
+                <br/>Stay Relaxed</h2>
                 <Link to='/todo-list'><button className="btn main-page-btn">Get Started</button></Link>
             </div>
             <img src="../img/productive.png" alt="Check List" className="check-list-img"/>
